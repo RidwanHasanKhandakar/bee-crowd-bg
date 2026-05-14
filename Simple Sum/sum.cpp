8ed154a7373dd054fceaebc5d437550045a72cbd
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-15 01:19:16 +0600

    did simple sum in cpp

---
+++ Simple Sum/sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    int SOMA;
+    SOMA=a+b;
+    cout<<"SOMA = "<<SOMA<<endl;
+    
+    return 0;
+    }
