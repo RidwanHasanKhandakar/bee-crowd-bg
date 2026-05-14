11a144a057c8a41750739a71d605b4c08962eb5d
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-15 01:09:45 +0600

    did hello in cpp & c#

---
+++ hello world/hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
